5ab6fc396c16724290415c6cc78aa7eeea042a0d
Devil39 <vvgyshj@gmail.com>
2024-09-11 08:11:27 +0530

    Commmands to commands

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = 'YOUR_API_ID'
-api_hash = 'YOUR_API_HASH'
-bot_token = 'YOUR_BOT_TOKEN'
-owner_id = 'YOUR_TELEGRAM_USER_ID'  # Replace with your Telegram user ID
-sudo_users = ['SUDO_USER_ID1', 'SUDO_USER_ID2']  # Replace with additional sudo user IDs
+api_id = '24572098'
+api_hash = '3a5a618283d80ef2bdd00d6bd4f5d9e8'
+bot_token = '7038034051:AAFB_wt-7aO0P3cI2X_S6ZjeeI3m3UFejYs'
+owner_id = '6014072313'  # Replace with your Telegram user ID
+sudo_users = ['5005777092', 'SUDO_USER_ID2']  # Replace with additional sudo user IDs
